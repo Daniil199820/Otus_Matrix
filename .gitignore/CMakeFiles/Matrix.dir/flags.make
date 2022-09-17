5c19f85f965f6a0cd769ff46fa012dc0792607f0
Daniil <dry.ginpost@gmail.com>
2022-09-17 17:27:32 +0300

    correct actions

--- .gitignore/CMakeFiles/Matrix.dir/flags.make
+++ .gitignore/CMakeFiles/Matrix.dir/flags.make
@@ -4,7 +4,7 @@
 # compile CXX with /usr/bin/c++
 CXX_DEFINES = 
 
-CXX_INCLUDES = -I/home/daniil/cpp/Otus_Matrix
+CXX_INCLUDES = 
 
 CXX_FLAGS = -Wall -Wextra -pedantic -Werror -std=gnu++17
 
--- .gitignore/CTestTestfile.cmake
+++ .gitignore/CTestTestfile.cmake
@@ -5,4 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(TEST_lib "TEST_lib")
-set_tests_properties(TEST_lib PROPERTIES  _BACKTRACE_TRIPLES "/home/daniil/cpp/Otus_Matrix/CMakeLists.txt;67;add_test;/home/daniil/cpp/Otus_Matrix/CMakeLists.txt;0;")
+set_tests_properties(TEST_lib PROPERTIES  _BACKTRACE_TRIPLES "/home/daniil/cpp/Otus_Matrix/CMakeLists.txt;63;add_test;/home/daniil/cpp/Otus_Matrix/CMakeLists.txt;0;")
